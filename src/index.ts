86b037046ec4bf42b4fd23042ff86bfc64034c7a
Diogo Manhaes <dsmanhaes@gmail.com>
2025-01-20 18:21:41 -0300

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import { todoRouter } from "./todoRouter";
+import cors from "cors";
 
 const app = express();
 const port = 7007;
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send(`Try access <a href="http://localhost:${port}/todo">/todo</a> endpoint`);
+app.get("/", (_req, res) => {
+  res.send(
+    `Try access <a href="http://localhost:${port}/todo">/todo</a> endpoint`
+  );
 });
 
-app.use('/todo', todoRouter);
+app.use("/todo", todoRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
